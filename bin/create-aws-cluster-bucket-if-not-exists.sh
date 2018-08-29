79c2166bd3a890db9b558349cb0cb8f98aa916e9
clevandowski <cyrille.levandowski@free.fr>
2018-08-29 19:57:11 +0200

    Le nom du bucket S3 utilise maintenant du nom du groupe comme préfixe plutot que le nom du user afin de faciliter l'utilisation du cluster multiuser

--- bin/create-aws-cluster-bucket-if-not-exists.sh
+++ bin/create-aws-cluster-bucket-if-not-exists.sh
@@ -5,18 +5,23 @@ if [ -z "$KOPS_USER" ]; then
   exit 1
 fi
 
+if [ -z "$KOPS_GROUP" ]; then
+  echo "Environment variable KOPS_GROUP must be defined. Aborting"
+  exit 1
+fi
+
 while ! aws s3api --profile $KOPS_USER list-buckets --output text >.buckets 2>/dev/null; do
   echo "Waiting for AWS access key user registration"
   sleep 1
 done
 
-if cat .buckets | grep $KOPS_USER-state-store >/dev/null; then
-  echo "AWS S3 bucket $KOPS_USER-state-store already exists"
+if cat .buckets | grep $KOPS_GROUP-state-store >/dev/null; then
+  echo "AWS S3 bucket $KOPS_GROUP-state-store already exists"
 else
-  echo "Creating AWS S3 bucket $KOPS_USER-state-store"
+  echo "Creating AWS S3 bucket $KOPS_GROUP-state-store"
 
   while ! aws --profile $KOPS_USER s3api create-bucket \
-    --bucket $KOPS_USER-state-store \
+    --bucket $KOPS_GROUP-state-store \
     --region $AWS_REGION \
     --create-bucket-configuration LocationConstraint=$AWS_REGION; do
     echo "Waiting for AWS access key user registration"
--- bin/source-kops-env.sh
+++ bin/source-kops-env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export AWS_PROFILE=$KOPS_USER
-export KOPS_STATE_STORE=s3://$KOPS_USER-state-store
+export KOPS_STATE_STORE=s3://$KOPS_GROUP-state-store
 export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
 export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
 export KOPS_STATE_STORE_AWS=$KOPS_STATE_STORE
-export KOPS_STATE_STORE_GCP=gs://$KOPS_USER-state-store
+export KOPS_STATE_STORE_GCP=gs://$KOPS_GROUP-state-store
