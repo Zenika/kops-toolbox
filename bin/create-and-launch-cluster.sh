752531a9155824fc550ad908b037a470b917d800
clevandowski <cyrille.levandowski@free.fr>
2018-09-11 22:29:17 +0200

    Ajout extension URL du dashboard dans les infos du cluster à la fin

--- bin/create-and-launch-cluster.sh
+++ bin/create-and-launch-cluster.sh
@@ -73,6 +73,7 @@ done
 
 echo -e "\n\n ////////////////////// \n \n all components deployed"
 echo -e "\n\n ////////////////////// \n \n $(kubectl cluster-info)"
+echo -e "\n\n ////////////////////// \n \n add '/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/' to dashboard URL"
 echo -e "\n\n ////////////////////// \n \n $(kops get secrets admin --type secret -oplaintext) is your admin user token"
 echo -e "\n\n ////////////////////// \n \n $(kops get secrets kube --type secret -oplaintext) is your kube user token \n\n ////////////////////// \n \n"
 
