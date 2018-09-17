3bc5839710868f4d754f6ceb89d3aef97511a85f
Rude-Monkey <vgilles@et.esiea.fr>
2018-09-17 15:38:13 +0200

    ajout d'un début de script d'installation pour Jenkins-X

---
+++ bin/install-jenkins-x.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+git config --global --add user.name "$GIT_USER_NAME"
+git config --global --add user.email "$GIT_USER_EMAIL"
+
+jx install --batch-mode \
+--provider aws \
+--headless \
+--git-provider-url https://github.com \
+--git-username Rude-Monkey \
+--git-api-token $GIT_API_TOKEN \
+--install-dependencies \
+--no-default-environments \
+--git-private
