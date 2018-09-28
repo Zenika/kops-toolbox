b53a363792c9d578e1b6507123d5f984e6e3cf5e
Rude-Monkey <vgilles@et.esiea.fr>
2018-09-28 15:53:47 +0200

    [ADD] adding the prometheus-operator yaml descriptor generator script

---
+++ bin/generate-prometheus-operator-descriptor.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo -e "Generating the prometheus-operator yaml descriptor with cluster domain $CLUSTER_DOMAIN"
+
+cat ~/res/addons/prometheus/prometheus-operator-first-part.yaml > ~/res/addons/prometheus/prometheus-operator-descriptor.yaml
+~/res/addons/prometheus/prometheus-operator-ingress.sh >> ~/res/addons/prometheus/prometheus-operator-descriptor.yaml
+
